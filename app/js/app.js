64cf43d5fa98eafa6d43c086c22263d62f8c0d74
tjabbe <martin.goutry@gmail.com>
2017-03-26 07:49:04 +0200

    feat(scrollto): add scrollto animation on click

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,15 @@
 // import styles to inject it to the head tag
 import styles from '../sass/app.scss'
 
+import TweenLite from 'gsap'
+import ScrollToPlugin from 'gsap/ScrollToPlugin'
+import ScrollMagic from 'scrollmagic'
+
 class Application {
     
   constructor() {
+    this.anchors  = document.querySelectorAll('.intro__anchor')
+
     this._init()
   }
 
@@ -12,11 +18,19 @@ class Application {
   }
 
   _initEvents() {
-    console.log('working')
+    this.anchors.forEach(anchor => {
+      anchor.addEventListener('click', this._scrollToElem.bind(this))
+    })
+  }
+
+  _scrollToElem(e) {
+    const targetScroll = e.target.dataset.scroll
+
+    TweenLite.to(window, 1, {scrollTo: document.querySelector(`.${targetScroll}`), ease: Power1.easeOut})
   }
 
 }
 
 document.addEventListener("DOMContentLoaded", function() { 
   const app = new Application()
-});
+})
